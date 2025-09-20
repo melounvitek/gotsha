8417b56a23814ea85f070edb6fd5adc4e9c70428
Vitek <vitek@meloun.info>
2025-09-20 23:31:02 +0200

    Rubocop

--- lib/gotsha/actions/verify.rb
+++ lib/gotsha/actions/verify.rb
@@ -9,7 +9,10 @@ module Gotsha
         last_commit_note =
           BashCommand.run!("git --no-pager notes --ref=gotsha show #{last_commit_sha}").text_output
 
-        raise(Errors::HardFail, "not verified yet") if last_commit_note.start_with?("error: no note found") || last_commit_note.to_s.empty?
+        if last_commit_note.start_with?("error: no note found") || last_commit_note.to_s.empty?
+          raise(Errors::HardFail,
+                "not verified yet")
+        end
         raise(Errors::HardFail, "tests failed") if last_commit_note.start_with?("Tests failed:")
         raise(Errors::HardFail, "uknown note content") unless last_commit_note.start_with?("Tests passed:")
 
