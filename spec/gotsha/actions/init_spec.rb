4a429cced8de713758eecc640a425ece91b2870b
Vitek <vitek@meloun.info>
2025-09-20 23:02:52 +0200

    New init test

--- spec/gotsha/actions/init_spec.rb
+++ spec/gotsha/actions/init_spec.rb
@@ -6,26 +6,52 @@ RSpec.describe Gotsha::Actions::Init do
   end
 
   describe "init" do
-    before do
-      allow(File).to receive(:exist?).and_return(false)
+    context "first time" do
+      before do
+        allow(File).to receive(:exist?).and_return(false)
+      end
+
+      it "creates default files and Git configuration" do
+        expect(FileUtils).to receive(:mkdir_p).with(".gotsha/hooks")
+        expect(FileUtils).to receive(:mkdir_p).with(".gotsha")
+
+        expect(File).to receive(:write).with(Gotsha::CONFIG_FILE, File.read(Gotsha::CONFIG_TEMPLATE_PATH))
+        expect(File).to receive(:write).with(Gotsha::GH_CONFIG_FILE, File.read(Gotsha::GH_CONFIG_TEMPLATE_PATH))
+
+        expect(FileUtils).to receive(:cp).with(anything, ".gotsha/hooks/pre-push")
+        expect(FileUtils).to receive(:cp).with(anything, ".gotsha/hooks/post-commit")
+
+        expect(FileUtils).to receive(:chmod).with("+x", ".gotsha/hooks/pre-push")
+        expect(FileUtils).to receive(:chmod).with("+x", ".gotsha/hooks/post-commit")
+
+        expect(Kernel).to receive(:system).with("git config --local core.hooksPath .gotsha/hooks")
+
+        described_class.new.call
+      end
     end
 
-    it "creates default files and Git configuration" do
-      expect(FileUtils).to receive(:mkdir_p).with(".gotsha/hooks")
-      expect(FileUtils).to receive(:mkdir_p).with(".gotsha")
+    context "second time" do
+      before do
+        allow(File).to receive(:exist?).and_return(true)
+      end
 
-      expect(File).to receive(:write).with(Gotsha::CONFIG_FILE, File.read(Gotsha::CONFIG_TEMPLATE_PATH))
-      expect(File).to receive(:write).with(Gotsha::GH_CONFIG_FILE, File.read(Gotsha::GH_CONFIG_TEMPLATE_PATH))
+      it "creates default files and Git configuration" do
+        expect(FileUtils).to receive(:mkdir_p).with(".gotsha/hooks")
+        expect(FileUtils).not_to receive(:mkdir_p).with(".gotsha")
 
-      expect(FileUtils).to receive(:cp).with(anything, ".gotsha/hooks/pre-push")
-      expect(FileUtils).to receive(:cp).with(anything, ".gotsha/hooks/post-commit")
+        expect(File).not_to receive(:write).with(Gotsha::CONFIG_FILE, File.read(Gotsha::CONFIG_TEMPLATE_PATH))
+        expect(File).to receive(:write).with(Gotsha::GH_CONFIG_FILE, File.read(Gotsha::GH_CONFIG_TEMPLATE_PATH))
 
-      expect(FileUtils).to receive(:chmod).with("+x", ".gotsha/hooks/pre-push")
-      expect(FileUtils).to receive(:chmod).with("+x", ".gotsha/hooks/post-commit")
+        expect(FileUtils).not_to receive(:cp).with(anything, ".gotsha/hooks/pre-push")
+        expect(FileUtils).not_to receive(:cp).with(anything, ".gotsha/hooks/post-commit")
 
-      expect(Kernel).to receive(:system).with("git config --local core.hooksPath .gotsha/hooks")
+        expect(FileUtils).not_to receive(:chmod).with("+x", ".gotsha/hooks/pre-push")
+        expect(FileUtils).not_to receive(:chmod).with("+x", ".gotsha/hooks/post-commit")
 
-      described_class.new.call
+        expect(Kernel).to receive(:system).with("git config --local core.hooksPath .gotsha/hooks")
+
+        described_class.new.call
+      end
     end
   end
 end
