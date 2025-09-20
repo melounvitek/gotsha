ed4ad18dc1a0ccfce4f107857bdba27869a03665
Vitek <vitek@meloun.info>
2025-09-20 22:53:40 +0200

    Run

--- spec/gotsha_spec.rb
+++ spec/gotsha_spec.rb
@@ -5,63 +5,6 @@ RSpec.describe Gotsha::ActionDispatcher do
     allow($stdout).to receive(:puts)
   end
 
-  describe "run" do
-    context "without test command configured" do
-      before do
-        allow(Gotsha::Config::USER_CONFIG)
-          .to receive(:fetch)
-          .with("commands")
-          .and_return([])
-      end
-
-      it "fails with proper error" do
-        expect do
-          described_class.call(:run)
-        end.to raise_exception(
-          Gotsha::Errors::HardFail,
-          "please, define some test commands in `.gotsha/config.yml`"
-        )
-      end
-    end
-
-    context "with a test command configured" do
-      let(:test_command) { "rails t" }
-      let(:test_text_response) { "all went well" }
-      let(:sha) { "test-sha" }
-
-      before do
-        allow(Gotsha::Config::USER_CONFIG)
-          .to receive(:fetch)
-          .with("commands")
-          .and_return([test_command])
-
-        allow(Gotsha::Config::USER_CONFIG)
-          .to receive(:fetch)
-          .with("interrupt_push_on_tests_failure")
-          .and_return(false)
-      end
-
-      it "runs the command" do
-        allow_any_instance_of(described_class)
-          .to receive(:last_commit_sha)
-          .and_return(sha)
-
-        expect(Gotsha::BashCommand)
-          .to receive(:run!)
-          .with(test_command)
-          .and_return(double("bash_response", "success?" => true, "text_output" => "test_text_response"))
-
-        b64 = ["test_text_response"].pack("m0")
-        esc = b64.gsub("'", %q('"'"'))
-
-        expect(Gotsha::BashCommand)
-          .to receive(:silent_run!)
-          .with("PAGER=cat GIT_PAGER=cat sh -c 'printf %s \"#{esc}\" | base64 -d | git notes --ref=gotsha add -f -F -'")
-
-        described_class.call(:run)
-      end
-    end
-  end
 
   describe "verify" do
     let(:last_sha) { "sha_test" }
