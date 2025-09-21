47b3e97f80af2b89e024a7bf46be96698ea023b9
Vitek <vitek@meloun.info>
2025-09-21 09:11:21 +0200

    BashCommand tests

--- spec/gotsha/bash_command_spec.rb
+++ spec/gotsha/bash_command_spec.rb
@@ -1,14 +1,41 @@
 # frozen_string_literal: true
 
 RSpec.describe Gotsha::BashCommand do
+  describe ".run!" do
+    let(:pid)    { 123 }
+    let(:reader) { StringIO.new("hello\nworld\n") }
+    let(:status) { instance_double(Process::Status, success?: true) }
+
+    before do
+      allow(PTY).to receive(:spawn).and_yield(reader, anything, pid)
+      allow(Process).to receive(:wait2).with(pid).and_return([nil, status])
+    end
+
+    context "when verbose output disabled" do
+      before { allow(Gotsha::UserConfig).to receive(:get).with(:verbose).and_return(false) }
+
+      it "returns collected stdout and successful status" do
+        result = described_class.run!("echo test")
+
+        expect(result.text_output).to eq("hello\nworld")
+        expect(result.success?).to be(true)
+      end
+    end
+
+    context "when verbose output enabled" do
+      before { allow(Gotsha::UserConfig).to receive(:get).with(:verbose).and_return(true) }
+
+      it "prints command and output, and returns the result" do
+        expect { described_class.run!("echo test") }
+          .to output(/echo test\nhello\nworld\n/)
+          .to_stdout_from_any_process
+      end
+    end
+  end
+
   describe ".silent_run!" do
     context "when verbose output disabled" do
-      before do
-        allow(Gotsha::UserConfig)
-          .to receive(:get)
-          .with(:verbose)
-          .and_return(false)
-      end
+      before { allow(Gotsha::UserConfig).to receive(:get).with(:verbose).and_return(false) }
 
       let(:command) { "ls" }
 
@@ -20,12 +47,7 @@ RSpec.describe Gotsha::BashCommand do
     end
 
     context "when verbose output enabled" do
-      before do
-        allow(Gotsha::UserConfig)
-          .to receive(:get)
-          .with(:verbose)
-          .and_return(true)
-      end
+      before { allow(Gotsha::UserConfig).to receive(:get).with(:verbose).and_return(true) }
 
       let(:command) { "ls" }
 
