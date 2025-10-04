963239afcb3883dc5b1e7584c0a573a2fde68330
Vítek Meloun <vitek@meloun.info>
2025-10-04 22:08:11 +0200

    Fix

--- lib/gotsha/actions/status.rb
+++ lib/gotsha/actions/status.rb
@@ -9,7 +9,6 @@ module Gotsha
         last_commit_note =
           BashCommand.run!("git --no-pager notes --ref=gotsha show #{last_commit_sha}").text_output
 
-
         if last_commit_note.start_with?("error: no note found") || last_commit_note.to_s.empty?
           raise(Errors::HardFail,
                 "not verified yet")
