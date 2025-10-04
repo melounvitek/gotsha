f15a00f25f4c0221810f6fa5427343401b2de155
Vít Meloun <vitek@meloun.info>
2025-10-04 14:01:59 +0200

    Small wording change (#30)

--- lib/gotsha/actions/run.rb
+++ lib/gotsha/actions/run.rb
@@ -12,7 +12,7 @@ module Gotsha
         run_commands!
         create_git_note!("Tests passed:")
 
-        "tests passed"
+        "commit verified"
       end
 
       private
@@ -34,9 +34,10 @@ module Gotsha
 
           next if command_result.success?
 
-          create_git_note!("Tests failed:")
-          puts command_result.text_output
-          raise fail_exception, "tests failed"
+          create_git_note!("Tests failed:\n\n")
+          puts command_result.text_output.split("\n").last(20).join("\n")
+          # puts "\n\nRun `gotsha show` for full output" TODO
+          raise fail_exception, "commit not verified"
         end
       end
 
