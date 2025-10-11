38f60b94899b333f2ca1a37057aee12f15ee2e00
Vitek <vitek@meloun.info>
2025-10-11 22:08:37 +0200

    COmment

--- lib/gotsha/user_config.rb
+++ lib/gotsha/user_config.rb
@@ -5,7 +5,8 @@ module Gotsha
     def self.get(key)
       config = TomlRB.load_file(Config::CONFIG_FILE).transform_keys(&:to_sym)
 
-      ENV[key.to_s.upcase] || config[key]
+      ENV["GOTSHA_#{key.to_s.upcase}"] || # this allows changing config via ENV vars
+        config[key]
     rescue Errno::ENOENT
       nil
     end
