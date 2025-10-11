b1f639b227b4bd04eec9f16fd6269d684c180fa6
Vitek <vitek@meloun.info>
2025-10-11 22:45:03 +0200

    run -> test

--- spec/gotsha/actions/commit_spec.rb
+++ spec/gotsha/actions/commit_spec.rb
@@ -2,12 +2,12 @@
 
 RSpec.describe Gotsha::Actions::Commit do
   describe "commit" do
-    it "creates a blank commit and triggers `run` on it" do
+    it "creates a blank commit and triggers `test` on it" do
       expect(Gotsha::BashCommand)
         .to receive(:silent_run!)
         .with("git -c core.hooksPath=/dev/null commit --allow-empty -m \"Run Gotsha\"")
 
-      expect_any_instance_of(Gotsha::Actions::Run).to(receive(:call))
+      expect_any_instance_of(Gotsha::Actions::Test).to(receive(:call))
 
       described_class.new.call
     end
