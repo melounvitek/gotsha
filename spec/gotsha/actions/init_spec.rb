7171fdae0b4197bbe6be464ff5ebdd4b8397ad95
Vitek <vitek@meloun.info>
2025-10-11 23:10:02 +0200

    Fix

--- spec/gotsha/actions/init_spec.rb
+++ spec/gotsha/actions/init_spec.rb
@@ -37,18 +37,18 @@ RSpec.describe Gotsha::Actions::Init do
         allow(File).to receive(:exist?).and_return(true)
       end
 
-      it "creates default files and Git configuration" do
-        expect(FileUtils).to receive(:mkdir_p).with(".gotsha/hooks")
+      it "does not create any files; onle Git configuration" do
+        expect(FileUtils).not_to receive(:mkdir_p).with(".gotsha/hooks")
         expect(FileUtils).not_to receive(:mkdir_p).with(".gotsha")
 
         expect(File).not_to receive(:write).with(Gotsha::CONFIG_FILE, File.read(Gotsha::CONFIG_TEMPLATE_PATH))
         expect(File).not_to receive(:write).with(Gotsha::GH_CONFIG_FILE, File.read(Gotsha::GH_CONFIG_TEMPLATE_PATH))
 
-        expect(FileUtils).to receive(:cp).with(anything, ".gotsha/hooks/pre-push")
-        expect(FileUtils).to receive(:cp).with(anything, ".gotsha/hooks/post-commit")
+        expect(FileUtils).not_to receive(:cp).with(anything, ".gotsha/hooks/pre-push")
+        expect(FileUtils).not_to receive(:cp).with(anything, ".gotsha/hooks/post-commit")
 
-        expect(FileUtils).to receive(:chmod).with("+x", ".gotsha/hooks/pre-push")
-        expect(FileUtils).to receive(:chmod).with("+x", ".gotsha/hooks/post-commit")
+        expect(FileUtils).not_to receive(:chmod).with("+x", ".gotsha/hooks/pre-push")
+        expect(FileUtils).not_to receive(:chmod).with("+x", ".gotsha/hooks/post-commit")
 
         expect(Kernel).to receive(:system).with("git config --local core.hooksPath .gotsha/hooks")
 
