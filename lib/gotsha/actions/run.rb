cb70e53ab62f0b15d97d50e2ad348f1ccd9479e7
Vít Meloun <vitek@meloun.info>
2025-10-11 21:42:09 +0200

    Ensure colors get stored even when tests run via Docker (#34)

--- lib/gotsha/actions/run.rb
+++ lib/gotsha/actions/run.rb
@@ -3,7 +3,8 @@
 module Gotsha
   module Actions
     class Run
-      TESTS_FAILED_NOTE_PREFIX = "Tests failed:\n\n"
+      TESTS_PASSED_NOTE_PREFIX = "Tests passed:"
+      TESTS_FAILED_NOTE_PREFIX = "Tests failed:"
 
       def initialize
         @tests_text_outputs = []
@@ -12,7 +13,7 @@ module Gotsha
       def call
         ensure_commands_defined!
         run_commands!
-        create_git_note!("Tests passed:")
+        create_git_note!(TESTS_PASSED_NOTE_PREFIX)
 
         "commit verified"
       end
