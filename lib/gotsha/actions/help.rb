21fb7d4f477a44a9ba10ff27c60bef41f3f3106a
Vitek <vitek@meloun.info>
2025-10-13 21:15:25 +0200

    Shorten long lines

--- lib/gotsha/actions/help.rb
+++ lib/gotsha/actions/help.rb
@@ -30,14 +30,18 @@ module Gotsha
       def config_file
         [
           "Config file:",
-          "How and when Gotsha runs tests is configured in `#{Config::CONFIG_FILE}` file, so it's the most important file to check and understand! Luckily, it's brief and contains explaining comments. If you deleted the comments it was originally generated with, or you're not sure how to set it, see https://github.com/melounvitek/gotsha/blob/master/lib/gotsha/templates/config.toml\n"
+          "How and when Gotsha runs tests is configured in `#{Config::CONFIG_FILE}` file, " \
+          "so it's the most important file to check and understand! Luckily, it's brief " \
+          "and contains explaining comments. If you deleted the comments it was originally " \
+          "generated with, or you're not sure how to set it, see " \
+          "https://github.com/melounvitek/gotsha/blob/master/lib/gotsha/templates/config.toml\n"
         ].join("\n\n")
       end
 
       def contact
         [
           "Contact:",
-          "Is something not clear? Did you find a bug? Would you use new feature? Let's talk!",
+          "Is something not clear? Did you find a bug? Would you use new feature? Let's talk! \n" \
           "Freel free to email me (vitek@meloun.info), or create an issue (https://github.com/melounvitek/gotsha/issues/)"
         ]
       end
