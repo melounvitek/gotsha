4b6da942c250181825c3295f7979e116ab3a03d1
Vítek Meloun <vitek@meloun.info>
2025-10-25 13:51:06 +0200

    Less intrusive GitLab config file

--- lib/gotsha/config.rb
+++ lib/gotsha/config.rb
@@ -7,7 +7,7 @@ module Gotsha
     CONFIG_TEMPLATE_PATH = File.expand_path("templates/config.toml", __dir__)
     GH_CONFIG_FILE = File.join(".github", "workflows", "gotsha.yml")
     GH_CONFIG_TEMPLATE_PATH = File.expand_path("templates/github_action_example.yml", __dir__)
-    GL_CONFIG_FILE = ".gitlab-ci.yml"
+    GL_CONFIG_FILE = ".gitlab-ci.gotsha.yml"
     GL_CONFIG_TEMPLATE_PATH = File.expand_path("templates/gitlab_action_example.yml", __dir__)
     HOOKS_TEMPLATES_DIR = File.expand_path("templates", __dir__)
     HOOKS_DIR = File.join(CONFIG_DIR, "hooks")
