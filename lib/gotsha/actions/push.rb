b1067f888423c58bc8fdcfeab48b5b3805598486
Vítek Meloun <vitek@meloun.info>
2025-10-30 13:17:18 +0100

    Force push every time

--- lib/gotsha/actions/push.rb
+++ lib/gotsha/actions/push.rb
@@ -6,22 +6,9 @@ module Gotsha
       DESCRIPTION = "pushes Gotsha test results to remote"
 
       def call
-        try_push = push_command
-
-        unless try_push.success?
-          puts "First push detected, need to run tests one more time..."
-          Fetch.new.call
-          Test.new.call
-          push_command
-        end
+        BashCommand.silent_run!("git push --no-verify --force origin refs/notes/gotsha:refs/notes/gotsha")
 
         "pushed"
-      end
-
-      private
-
-      def push_command
-        BashCommand.silent_run!("git push --no-verify origin refs/notes/gotsha:refs/notes/gotsha")
       end
     end
   end
