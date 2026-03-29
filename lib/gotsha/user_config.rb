e6b5d3ffcc6ffa9ea1378809301cc85047749ba9
Vítek Meloun <vitek@meloun.info>
2026-03-29 15:34:11 +0000

    Coerce scalar config ENV overrides

--- lib/gotsha/user_config.rb
+++ lib/gotsha/user_config.rb
@@ -4,13 +4,24 @@ module Gotsha
   class UserConfig
     def self.get(key)
       config = new.to_h
+      env_key = "GOTSHA_#{key.to_s.upcase}"
+      env_value = ENV[env_key]
 
-      ENV["GOTSHA_#{key.to_s.upcase}"] || # this allows changing config via ENV vars
-        config[key]
+      return config[key] if key.to_sym == :commands
+      return coerce_env_value(env_value) unless env_value.nil?
+
+      config[key]
     end
 
     def self.blank?
       new.to_h.empty?
+    end
+
+    def self.coerce_env_value(value)
+      return true if value == "true"
+      return false if value == "false"
+
+      value
     end
 
     def to_h
--- spec/gotsha/user_config_spec.rb
+++ spec/gotsha/user_config_spec.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 RSpec.describe Gotsha::UserConfig do
+  around do |example|
+    original_env = ENV.to_hash
+    example.run
+    ENV.replace(original_env)
+  end
+
   describe "when the config file does not exists" do
     before do
       stub_const "Gotsha::Config::CONFIG_FILE", "not/exists"
@@ -28,6 +34,33 @@ RSpec.describe Gotsha::UserConfig do
       expect(described_class.blank?).to eq(false)
       expect(described_class.get(:unexisting_key)).to eq(nil)
       expect(described_class.get(:test_key)).to eq("test_value")
+    end
+  end
+
+  describe "when ENV overrides are used" do
+    before do
+      allow(TomlRB)
+        .to receive(:load_file)
+        .with(Gotsha::Config::CONFIG_FILE)
+        .and_return({ "verbose" => true, "commands" => ["bin/rspec"] })
+    end
+
+    it "coerces false boolean values" do
+      ENV["GOTSHA_VERBOSE"] = "false"
+
+      expect(described_class.get(:verbose)).to eq(false)
+    end
+
+    it "coerces true boolean values" do
+      ENV["GOTSHA_CI"] = "true"
+
+      expect(described_class.get(:ci)).to eq(true)
+    end
+
+    it "does not override commands via ENV string values" do
+      ENV["GOTSHA_COMMANDS"] = "bin/test"
+
+      expect(described_class.get(:commands)).to eq(["bin/rspec"])
     end
   end
 end
