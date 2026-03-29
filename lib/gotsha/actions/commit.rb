0921fe502c1cedef12a0e6f11a0ac18448983ebd
Vítek Meloun <vitek@meloun.info>
2026-03-29 15:29:39 +0000

    Stop sign-off after failed dummy commit

--- lib/gotsha/actions/commit.rb
+++ lib/gotsha/actions/commit.rb
@@ -6,7 +6,9 @@ module Gotsha
       DESCRIPTION = "runs tests on a dummy commit for manual sign-off"
 
       def call
-        BashCommand.silent_run!('git -c core.hooksPath=/dev/null commit --allow-empty -m "Run Gotsha"')
+        command = BashCommand.silent_run!('git -c core.hooksPath=/dev/null commit --allow-empty -m "Run Gotsha"')
+
+        raise Errors::HardFail, "something went wrong" unless command.success?
 
         Test.new.call
       end
